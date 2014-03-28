70eb2fc9a8b8941713cef57ecf0bf25d3a4b8a77
poo <poo@PUEPUE-0T7UPU7Q.aula.local>
2014-03-28 14:37:23 +0100

    pe

--- PrestamosCD-GUI/src/BuscarReg.java
+++ PrestamosCD-GUI/src/BuscarReg.java
@@ -1,6 +1,10 @@
-
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.SwingUtilities;
 
 
@@ -22,6 +26,13 @@ public class BuscarReg extends javax.swing.JDialog {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JButton jbtAceptar;
+	private JButton jbtBorrar;
+	private JScrollPane jScrollPane1;
+	private JList jlsListaCDs;
+	private JButton jbtCancelar;
+	
+	Cd ventanaPadre;
 
 	/**
 	* Auto-generated main method to display this JDialog
@@ -39,16 +50,53 @@ public class BuscarReg extends javax.swing.JDialog {
 	public BuscarReg(JFrame frame) {
 		super(frame);
 		initGUI();
+		ventanaPadre=(Cd)frame;
+		jlsListaCDs.setListData(ventanaPadre.getCDs());	
 	}
 	
 	private void initGUI() {
 		try {
+			
 			{
 				this.setResizable(false);
 				getContentPane().setLayout(null);
 				this.setTitle("Buscar Registro");
+				{
+					jbtAceptar = new JButton();
+					getContentPane().add(jbtAceptar);
+					jbtAceptar.setText("Aceptar");
+					jbtAceptar.setBounds(285, 21, 90, 26);
+					jbtAceptar.setMnemonic(java.awt.event.KeyEvent.VK_A);
+				}
+				{
+					jbtCancelar = new JButton();
+					getContentPane().add(jbtCancelar);
+					jbtCancelar.setText("Cancelar");
+					jbtCancelar.setBounds(285, 58, 90, 26);
+					jbtCancelar.setMnemonic(java.awt.event.KeyEvent.VK_C);
+				}
+				{
+					jbtBorrar = new JButton();
+					getContentPane().add(jbtBorrar);
+					jbtBorrar.setText("Borrar");
+					jbtBorrar.setBounds(285, 95, 90, 26);
+					jbtBorrar.setMnemonic(java.awt.event.KeyEvent.VK_B);
+				}
+				{
+					jScrollPane1 = new JScrollPane();
+					getContentPane().add(jScrollPane1);
+					jScrollPane1.setBounds(17, 21, 247, 220);
+					{
+			ListModel jlsListaCDsModel=	new DefaultComboBoxModel(new String[] {""});
+						jlsListaCDs = new JList();
+						jScrollPane1.setViewportView(jlsListaCDs);
+						jlsListaCDs.setModel(jlsListaCDsModel);
+					}
+					jScrollPane1.setViewportView(jlsListaCDs);
+				}
 			}
 			setSize(400, 300);
+			getRootPane().setDefaultButton(jbtAceptar);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
